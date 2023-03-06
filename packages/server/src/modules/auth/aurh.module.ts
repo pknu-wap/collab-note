e269e6184a97b056bd838315a78520019681a5be
김민수 MINSOO KIM <alstn113@gmail.com>
2023-03-07 08:20:52 +0900

    fix: auth service injection (#1)

--- packages/server/src/modules/auth/aurh.module.ts
+++ packages/server/src/modules/auth/aurh.module.ts
@@ -7,5 +7,6 @@ import { AuthService } from './auth.service';
   imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
