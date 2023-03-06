c3e0767ef4c0a9bd6d0c2d0a7f33e057ed4c7706
김민수 MINSOO KIM <alstn113@gmail.com>
2023-03-07 08:40:55 +0900

    fix: github strategy injection (#1)

--- packages/client/src/pages/HomePage/HomePage.tsx
+++ packages/client/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const onClick = () => {
+    window.location.href = 'http://localhost:8080/auth/github';
+  };
+  return (
+    <div>
+      <button onClick={onClick}>asdf</button>
+    </div>
+  );
 };
 
 export default HomePage;
--- packages/server/src/modules/auth/aurh.module.ts
+++ packages/server/src/modules/auth/aurh.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { GithubStrategy } from './strategies';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GithubStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
