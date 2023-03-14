a5ec0aa46e1eba889087399175c7de72b36dff7c
김민수 MINSOO KIM <alstn113@gmail.com>
2023-03-14 14:42:56 +0900

    feat: storybook eslint plugin

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
     'plugin:prettier/recommended',
   ],
   overrides: [],
