7218fb4db3ff83cbb2ab0b83be9208443a71954d
김민수 MINSOO KIM <alstn113@gmail.com>
2023-03-18 16:59:01 +0900

    feat: reduce cluster

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'collab-note-server', // pm2 name // 여러 개 생성 시 구분하기 위한 이름
       script: './dist/main.js', // // 앱 실행 스크립트
-      instances: 3, // 클러스터 모드 사용 시 생성할 인스턴스 수
+      instances: 1, // 클러스터 모드 사용 시 생성할 인스턴스 수
       exec_mode: 'cluster', // fork, cluster 모드 중 선택
       merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
       autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
