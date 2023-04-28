3f3f504280e20b9b1de12ce46180c6f9140d3768
김민수 MINSOO KIM <alstn113@gmail.com>
2023-04-28 20:57:30 +0900

    feat(client): add carousel component to comments (#33)

--- packages/client/src/components/common/Carousel/Carousel.tsx
+++ packages/client/src/components/common/Carousel/Carousel.tsx
@@ -14,6 +14,9 @@ interface ButtonVisible {
 
 type ScrollDirection = 'prev' | 'next';
 
+/**
+ * @param children Carousel에 List 컴포넌트를 넣는다.
+ */
 export const Carousel = ({ children }: Props) => {
   const scrollPanelRef = useRef<HTMLDivElement>(null);
   const listRef = useRef<HTMLDivElement>(null);
@@ -23,15 +26,17 @@ export const Carousel = ({ children }: Props) => {
     next: true,
   });
 
+  /** list의 첫번째 자식 widthg 저장 후 스크롤 이동에 사용한다. */
   const childElementWidth = useRef<number>(0);
 
+  /** scroll위치에 따라서 prev, next 버튼을 보여줄지 결정한다. */
   const updateButtonVisible = () => {
     if (!scrollPanelRef.current) return;
 
     const { scrollWidth, scrollLeft, clientWidth } = scrollPanelRef.current;
 
     const isPrevButtonVisible = scrollLeft <= 0;
-    const isNextButtonVisible = scrollWidth - clientWidth <= scrollLeft;
+    const isNextButtonVisible = scrollWidth - clientWidth <= scrollLeft; // clientWidth는 스크롤 패널의 너비
 
     setButtonVisible({
       prev: isPrevButtonVisible,
@@ -39,6 +44,7 @@ export const Carousel = ({ children }: Props) => {
     });
   };
 
+  // children이 바뀔 때마다 스크롤 위치를 초기화하고 버튼을 업데이트한다.
   useEffect(() => {
     if (!scrollPanelRef.current) return;
 
@@ -46,13 +52,16 @@ export const Carousel = ({ children }: Props) => {
     updateButtonVisible();
   }, [children]);
 
+  // children이 바뀔 때마다 첫 번째 자식 엘리먼트의 너비를 업데이트한다.
   useEffect(() => {
     const $firstChildElement = listRef.current?.firstElementChild;
 
     if (!$firstChildElement) return;
+    // 첫번째 element의 너비를 저장한 후 나중에 버튼 클릭시 스크롤 이동에 사용한다.
     childElementWidth.current = $firstChildElement.clientWidth;
   }, []);
 
+  /** prev or next 버튼을 클릭했을 때 스크롤 위치를 업데이트한다. */
   const handleChangeScroll = (direction: ScrollDirection) => () => {
     if (!scrollPanelRef.current) return;
 
@@ -61,11 +70,14 @@ export const Carousel = ({ children }: Props) => {
       direction === 'prev'
         ? scrollLeft - childElementWidth.current
         : scrollLeft + childElementWidth.current;
+
+    // 스크롤 위치가 0보다 작거나, 스크롤 위치가 마지막 위치보다 클 경우 스크롤을 이동하지 않는다.
     const isMoveable =
       scrollWidth - clientWidth + childElementWidth.current >= scrollLocation;
 
     if (!isMoveable) return;
 
+    // 스크롤 위치를 업데이트한다.
     scrollPanelRef.current.scrollTo(scrollLocation, 0);
   };
 
