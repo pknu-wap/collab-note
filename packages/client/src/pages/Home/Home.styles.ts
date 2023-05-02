f20c02658480adf52a95e24d3315a20eeb6aab21
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-02 20:50:27 +0900

    feat(client): add carousel in home page (#43)

--- packages/client/src/pages/Home/Home.styles.ts
+++ packages/client/src/pages/Home/Home.styles.ts
@@ -1,1 +1,9 @@
-export {};
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 1rem;
+`;
